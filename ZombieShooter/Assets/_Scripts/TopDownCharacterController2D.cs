2f67ecbe4397ef7f7d14f882eb3216aa475e738f
Alex "Splitz" Davis <32062643+Splitzy@users.noreply.github.com>
2018-02-26 13:13:12 +0000

    Week 1 Tutorial
    Completed Week 1's Tutorial and added some fixes to make the camera follow the player better and added boundaries for the player (also includes the art assets).

---
+++ ZombieShooter/Assets/_Scripts/TopDownCharacterController2D.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, yMin, yMax;
+}
+
+
+public class TopDownCharacterController2D : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody2D rb2D;
+    public Boundary boundary;
+
+	void Start ()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate ()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        rb2D.velocity = new Vector2(x, y) * speed;
+        rb2D.angularVelocity = 0.0f;
+
+        rb2D.position = new Vector2
+            (
+                Mathf.Clamp(rb2D.position.x, boundary.xMin, boundary.xMax),
+                Mathf.Clamp(rb2D.position.y, boundary.yMin, boundary.yMax)
+            );
+	}
+}
