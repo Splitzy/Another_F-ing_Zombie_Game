71585231306404b265c2b69bab4d82b4901d500c
Alex "Splitz" Davis <32062643+Splitzy@users.noreply.github.com>
2018-03-05 12:59:44 +0000

    Week 2 Completed
    Created the bullet prefabs, a zombie that follows you, a dark atmosphere with a flashlight and a rock prefab

---
+++ ZombieShooter/Assets/_Scripts/Bullet2D.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet2D : MonoBehaviour {
+
+    public float speed;
+    public float destroyTime;
+
+	void Start()
+    {
+        Invoke("Die", destroyTime);
+	}
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    void OnDestroy()
+    {
+        CancelInvoke("Die");
+    }
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody2D>().velocity = transform.up * speed;
+	}
+}
---
+++ ZombieShooter/Assets/_Scripts/ShootBullet.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBullet : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawnRight;
+    public Transform bulletSpawnLeft;
+    public float fireTime;
+
+    private bool isFiring = false;
+
+    void SetFiring()
+    {
+        isFiring = false;
+    }
+	
+    void Fire()
+    {
+        isFiring = true;
+        Instantiate(bulletPrefab, bulletSpawnRight.position, bulletSpawnRight.rotation);
+
+        Instantiate(bulletPrefab, bulletSpawnLeft.position, bulletSpawnLeft.rotation);
+
+        if(GetComponent<AudioSource>() != null)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+
+        Invoke("SetFiring", fireTime);
+    }
+
+	void Update ()
+    {
+		if(Input.GetMouseButton(0))
+        {
+            if(!isFiring)
+            {
+                Fire();
+            }
+        }
+	}
+}
