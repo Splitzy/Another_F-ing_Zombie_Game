b3606553db0ecd78a77fee2cc9420f23500f671d
Alex "Splitz" Davis <32062643+Splitzy@users.noreply.github.com>
2018-03-12 12:43:40 +0000

    Week 3 Completed
    Added animations for a blood splatter and started on the animations for the player shooting

---
+++ ZombieShooter/Assets/_Scripts/Explosion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+
+    public float destroyTime;
+
+	void Start()
+    {
+        Invoke("Die", destroyTime);
+	}
+	
+	// Update is called once per frame
+	void Die()
+    {
+        Destroy(gameObject);
+	}
+}
