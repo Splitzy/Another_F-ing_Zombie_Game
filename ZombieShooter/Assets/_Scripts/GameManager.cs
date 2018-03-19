712b97513491e50001cb2cddb52be2655689430a
Alex "Splitz" Davis <32062643+Splitzy@users.noreply.github.com>
2018-03-19 13:33:07 +0000

    Menus
    Created a Start menu, Pause menu and a game over menu

---
+++ ZombieShooter/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public void StartGame()
+    {
+        SceneManager.LoadScene("Zombie Shooter Level 1");
+    }
+
+    public void EndGame()
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+	
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void Continue()
+    {
+
+    }
+}
---
+++ ZombieShooter/Assets/_Scripts/PauseMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+	
+
+    void Start()
+    {
+        Resume();
+    }
+
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+	}
+
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        isPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0.0f;
+        isPaused = true;
+    }
+}
